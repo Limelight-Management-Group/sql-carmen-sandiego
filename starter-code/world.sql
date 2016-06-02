4b038eb22fa5c175505a65abb54286383891d45d
Wayne Banks <thestorerun@yahoo.com>
2016-06-01 21:17:59 -0700

    finished Step2

--- starter-code/world.sql
+++ starter-code/world.sql
@@ -13,7 +13,7 @@ CREATE TABLE city (
     id integer NOT NULL,
     name text NOT NULL,
     countrycode character(3) NOT NULL,
-    district text NOT NULL,
+      district text NOT NULL,
     population integer NOT NULL
 );
 
